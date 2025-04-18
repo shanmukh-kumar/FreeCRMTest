f933708864f3cb33670d16cb7b08bb3eb727abad
madhu77-cloud <chimuturimadhubabu59@gmail.com>
2025-04-18 07:47:52 +0530

    Formatted

--- src/main/java/com/demoqa/selenium/tests/buttonsTest.java
+++ src/main/java/com/demoqa/selenium/tests/buttonsTest.java
@@ -12,32 +12,33 @@ import com.demoqa.selenium.pages.buttonsPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class buttonsTest {
-	
-			@Test
-			public void testGoogle() throws InterruptedException {
-				WebDriverManager.chromedriver().setup();;
-				WebDriver driver =new ChromeDriver();
-				driver.manage().window().maximize();
-				driver.get("https://www.google.com/");
-				driver.findElement(By.name("q")).sendKeys("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-chrome-driver/4.29.0", Keys.ENTER);
-				System.out.println( driver.getTitle());
-				System.out.println( driver.getTitle());
-				Thread.sleep(1000);
-				driver.quit();
 
-				}
-			
-			public void testFacebook() throws InterruptedException {
-					WebDriverManager.chromedriver().setup();
-					WebDriver driver =new ChromeDriver();
-					driver.manage().window().maximize();
-					driver.get("https://www.facebook.com/");
-					driver.findElement(By.name("email")).sendKeys("901000815");
-					driver.findElement(By.name("pass")).sendKeys("madhu143");
-					driver.findElement(By.name("pass"));
-					System.out.println( driver.getTitle());
-					Thread.sleep(1000);
-					driver.quit();
-		}
+	@Test
+	public void testGoogle() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		;
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys(
+				"https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-chrome-driver/4.29.0", Keys.ENTER);
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		Thread.sleep(1000);
+		driver.quit();
+
 	}
 
+	public void testFacebook() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.name("email")).sendKeys("901000815");
+		driver.findElement(By.name("pass")).sendKeys("madhu143");
+		driver.findElement(By.name("pass"));
+		System.out.println(driver.getTitle());
+		Thread.sleep(1000);
+		driver.quit();
+	}
+}

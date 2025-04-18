d20245f29bb77c91eee157b8ec959229deee2067
madhu77-cloud <chimuturimadhubabu59@gmail.com>
2025-04-18 08:11:58 +0530

    Push testing

--- src/main/java/com/demoqa/selenium/tests/buttonsTest.java
+++ src/main/java/com/demoqa/selenium/tests/buttonsTest.java
@@ -3,11 +3,8 @@ package com.demoqa.selenium.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-import com.demoqa.selenium.pages.buttonsPage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 

08181da56049091b905be6218a9fc5412791a289
madhu77-cloud <chimuturimadhubabu59@gmail.com>
2025-04-18 08:11:58 +0530

    Push testing

--- src/main/java/com/demoqa/selenium/tests/buttonsTest.java
+++ src/main/java/com/demoqa/selenium/tests/buttonsTest.java
@@ -3,11 +3,8 @@ package com.demoqa.selenium.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-import com.demoqa.selenium.pages.buttonsPage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,7 +13,6 @@ public class buttonsTest {
 	@Test
 	public void testGoogle() throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
-		;
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
